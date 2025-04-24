b17472cbb52080f897e61476bd90dddcb88077ad
gmcguire <gmcguire@embedLinux.myguest.virtualbox.org>
2025-04-24 13:35:11 -0400

    trying again

---
+++ finder-app/finder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# set -x
+
+if [ $# -lt 2 ]
+then
+   echo "Not enough arguments."
+   exit 1
+elif [ $# -gt 2 ]
+then
+    echo "Too many arguments."
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ -d $filesdir ]
+then
+    results=$( grep -rch $searchstr $filesdir )
+else
+    echo $filesdir is not a directory
+fi 
+
+sum=$(echo "$results" | awk '{sum += $1; count++} END {print sum}')
+count=$(echo "$results" | wc -l)
+echo "The number of files are" $count "and the number of matching lines are" $sum 
---
+++ finder-app/writer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+    echo "Too few arguments. " $0 "is exiting."
+    exit 1
+elif [ $# -gt 2 ]
+then 
+    echo "Too many arguments. " $0 "is exiting.."
+    exit 1
+fi
+
+writefile=$1
+writestr=$2
+newpath="${writefile%/*}"
+mkdir -p ${newpath}
+if [ ! -d $newpath ]
+then
+    echo "directory not created!"
+    exit 1
+fi
+
+touch $writefile
+echo $writestr >> $writefile
