f0a17374fd7afdff6a42085e1cb79e3311b94e90
gmcguire <gmcguire@embedLinux.myguest.virtualbox.org>
2025-05-09 12:58:28 -0400

    Assignment 3B, attempt 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,12 +49,10 @@ then
 	fi
 fi
 #echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
 for i in $( seq 1 $NUMFILES)
 do
-	${WRITEDIR}/home/writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	/tmp/aeld/rootfs/home/writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
