cf125633487b6811fec6af1c00c695eeca4bf99a
gmcguire <gmcguire@embedLinux.myguest.virtualbox.org>
2025-05-11 15:19:56 -0400

    Assignment 4 part 1 completed

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h> // adding this for nanosleep system call
+#include <errno.h>
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
@@ -14,6 +15,47 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    // cast the void * parameter to its proper thread_data * type
+    DEBUG_LOG("ABOUT TO CAST THE thread_param TO A thread_data STRUCTURE");
+    struct thread_data * func_args = (struct thread_data *) thread_param;
+    if (func_args == NULL) {
+        perror("The cast of the parameter from void * to thread_data * failed.\n");
+        return thread_param;
+    }
+    // set up the waiting periods
+    printf("Pre-span milliseconds: %d \n", func_args->wait_to_obtain_ms);
+    printf("Post-span milliseconds: %d \n\n", func_args->wait_to_release_ms);
+    // Wait for the proper time
+    if (usleep(func_args->wait_to_obtain_ms*1000) == -1) {
+        DEBUG_LOG("USLEEP MESSED UP IN PRE SPAN");
+        return thread_param;
+    }
+   
+    // Get the mutex
+    DEBUG_LOG("ABOUT TO LOCK THE MUTECKS");
+    int arcc = pthread_mutex_lock(func_args->mutex);
+    if (arcc != 0 ) {
+        DEBUG_LOG("FAILED TO LOCK THE MOOTEX");
+        return thread_param;
+    }
+
+    // Wait for the proper time
+    if (usleep(func_args->wait_to_release_ms*1000) == -1) {
+        DEBUG_LOG("USLEEP MESSED UP IN POST SPAN");
+        return thread_param;
+    }
+
+    // Release the mutex
+    DEBUG_LOG("ABOUT TO UNLOCK THE MYOOTECKS");
+    arcc = pthread_mutex_unlock(func_args->mutex);
+    if (arcc != 0) {
+        DEBUG_LOG("FAILED TO UNLOCK THE MYOOTEX");
+        return thread_param;
+    }
+
+    DEBUG_LOG("IT SEEMS TO HAVE WORKED, BUT DID IT??");
+    func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +70,26 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    // Create the thread_data payload for the new thread
+    struct thread_data* payload = malloc(sizeof (struct thread_data));
+    if (payload == NULL) {
+        perror("Malloc for thread_data structure failed\n");
+        return false;
+    }
 
+    // we don't do the waiting in this function. it happens in the spawned function
+    // but we must initialize the thread_data object 
+    payload->wait_to_obtain_ms = wait_to_obtain_ms;
+    payload->wait_to_release_ms = wait_to_release_ms;
+    payload->thread_complete_success = false;
+    payload->mutex = mutex;
+
+    int new_thread = pthread_create(thread, NULL, threadfunc, (void*) payload);
+    if (new_thread != 0) {
+        perror("The pthread was not created.\n");
+        printf("Error reported: %d\n", errno);
+        free(payload); 
+        return false;
+    } 
+    return true;
+}
